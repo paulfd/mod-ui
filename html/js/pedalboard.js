d4c16e7c90dd6dbbde566dae787db1e53e25075e
Giedrius <giedrius@blokas.io>
2017-04-12 00:18:09 +0000

    When loading a bundle, issue a 'resetConnections' command to the browser.

--- html/js/pedalboard.js
+++ html/js/pedalboard.js
@@ -1727,6 +1727,14 @@ JqueryClass('pedalboard', {
         })
     },
 
+    resetConnections: function () {
+        var self = $(this)
+        var connMgr = self.data('connectionManager')
+        connMgr.iterate(function (jack) {
+            self.pedalboard('disconnect', jack)
+        })
+    },
+
     // Removes all pedalboard data
     resetData: function () {
         var self = $(this)

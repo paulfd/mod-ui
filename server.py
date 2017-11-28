7edc98689d833dc3a245d1bff4ce5f4dea01c9bc
Giedrius <giedrius@blokas.io>
2017-11-28 18:59:06 +0000

    Respect LV2_PEDALBOARDS_DIR env variable.

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ DATA_DIR = join(ROOT, 'data')
 os.makedirs(DATA_DIR, exist_ok=True)
 
 os.environ['MOD_DEV_ENVIRONMENT'] = os.environ.get("MOD_DEV_ENVIRONMENT", '1')
-os.environ['MOD_DATA_DIR'] = DATA_DIR
+#os.environ['MOD_DATA_DIR'] = DATA_DIR
 os.environ['MOD_LOG'] = os.environ.get("MOD_LOG", '1')
 os.environ['MOD_KEY_PATH'] = join(DATA_DIR, 'keys')
 os.environ['MOD_DEVICE_WEBSERVER_PORT'] = '8888'

888d39d9f10e201800894234567a6136c1d298ef
Giedrius <giedrius@blokas.io>
2020-01-29 11:47:44 +0000

    Hide Control Voltage UI elements

--- html/js/desktop.js
+++ html/js/desktop.js
@@ -729,6 +729,8 @@ function Desktop(elements) {
         $('#mod-cloud-plugins').hide()
         $('#mod-settings').hide()
         $('#pedalboard-info .js-cloud').hide()
+        $('#js-cv-addressing-toggle').hide()
+        $('#effect-tab-ControlVoltage').hide()
     }
 
     this.effectBox = self.makeEffectBox(elements.effectBox,
